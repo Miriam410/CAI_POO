d22aa7ff58245f112b8c51c0541305b5287e6fe8
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2020-09-21 12:40:38 -0300

    falta terminar

---
+++ CAI_RESTAURANT/CAI_RESTAURANT/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_RESTAURANT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Restaurant> ListaRest = new List<Restaurant>();
+
+            Restaurant R1 = new Restaurant(1, "Maria", "Lopez", 35, DateTime.Parse("21/09/2020 14:00:00"));
+            ListaRest.Add(R1);
+            R1.AgregarMesa();
+            R1.AgregarSilla();
+            R1.AgregarSilla();
+            R1.AgregarSilla();
+            R1.QuitarSilla(1);
+
+            Restaurant R2 = new Restaurant(1, "Lucia", "Lopez", 25, DateTime.Parse("21/09/2020 12:00:00"));
+            ListaRest.Add(R2);
+            R2.AgregarSilla();
+           
+            Restaurant R3 = new Restaurant(1, "Juan", "Perez", 40, DateTime.Parse("25/09/2020 09:00:00"));
+            ListaRest.Add(R3);
+            R3.AgregarSilla();
+            R3.AgregarSilla();
+           
+            Restaurant R4 = new Restaurant(1, "Carlos", "Fernandez", 60, DateTime.Parse("07/10/2020 21:00:00"));
+            ListaRest.Add(R4);
+           
+            Restaurant R5 = new Restaurant(1, "Mirta", "Duran", 58, DateTime.Parse("07/10/2020 15:00:00"));
+            ListaRest.Add(R5);
+
+          
+   
+
+            Console.ReadKey();
+        
+        }
+    }
+}
+
+
+    class Restaurant 
+    {
+        private int Reserva;
+        private string Nombre;
+        private string Apellido;
+        private int Edad;
+        private DateTime FechaHora;
+        private int Mesa;
+        private int Silla;
+        
+        public int reserva
+        {
+            get
+            {
+                return Reserva;
+            }
+            set
+            {
+                Reserva = value;
+            }
+        }
+
+        public string nombre
+        {
+            get
+            {
+                return Nombre;
+            }
+            set
+            {
+                Nombre = value;
+            }
+        }
+
+        public string apellido
+        {
+            get
+            {
+                return Apellido;
+            }
+            set
+            {
+                Apellido = value;
+            }
+        }
+
+        private int edad
+        {
+            get
+            {
+                return Edad;
+            }
+            set
+            {
+                Edad = value;
+            }
+        }
+        public DateTime fechahora
+        {
+            get 
+            {
+                return FechaHora;
+            }
+            set
+            {
+                FechaHora = value;
+            }
+        }
+
+
+        public Restaurant(int Reserva,string Nombre, string Apellido, int Edad, DateTime FechaHora)
+        {
+            this.Reserva = Reserva;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Edad = Edad;
+            this.FechaHora = FechaHora;
+            this.Mesa = 30;
+            this.Silla = 50;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2} - {3} - {4} ", this.Reserva, this.Nombre, this.Apellido,this.Edad, this.FechaHora);
+        }
+
+        public void AgregarMesa()
+        {
+            this.Mesa += 1;
+        }
+
+        public void AgregarSilla()
+        {
+            this.Silla += 1;
+        }
+        public void QuitarSilla(int Cantidad)
+        {
+            this.Silla = this.Silla - Cantidad;
+        }
+
+}
+
---
+++ CLASE_08_09_20202/CLASE_08_09_20202/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASE_08_09_20202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Auto> ListaAutos = new List<Auto>();
+            Auto a1 = new Auto("Ford", "Fiesta", "Nafta 1.4 TD");
+            ListaAutos.Add(a1);
+           // a1.QuitarStock(1, );
+
+            Auto a2 = new Auto("Chevrolet", "Onix", "Nafta 1.3 TD");
+            a2.AgregarUnAuto();
+            //a2.QuitarStock(11);    intercambiar con lìnea 28 para ver què hace
+            
+            ListaAutos.Add(a2);
+
+            ListaAutos.Add(new Auto("Volkswagen", "Golf", "Nafta 1.4 T5I "));
+             
+            Console.WriteLine("Ingrese el nombre de un auto");
+
+            Auto a = new Auto();
+            Auto a3 = a2;
+            a2.QuitarStock(11);
+            a3.AgregarUnAuto();
+
+            foreach (Auto auto in ListaAutos)
+            {
+                Console.WriteLine(auto.ToString());
+                if (auto.Vendido())
+                {
+                    Console.WriteLine("Esta vendido");
+                }
+                else
+                {
+                    Console.WriteLine("Hay stock");
+                }
+                //if (auto.Modelo.ToUpper().Contain(IngresoUsuario.ToUpper()))
+                //{
+                //    Console.WriteLine(auto.ToString());
+                //}
+            }
+         string IngresoUsuario = Console.ReadLine();
+        }
+}
+
+    public class Auto
+    {
+        private string marca;
+        private string modelo;
+        private string version;
+        private int stock;
+
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+            set
+            {
+                marca = value;
+            }
+
+        }
+
+        public string Modelo
+        {
+            get
+            {
+                return modelo;  
+            }
+            set
+            {
+                if (this.Marca == "ford")
+                {
+                    if (value == "fiesta")
+                    {
+                        modelo = value;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                else
+                {
+                    modelo = value;
+                }
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return version;
+            }
+        }
+
+        public Auto()
+        { 
+        }
+
+        public Auto(string marca, string modelo, string version)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.version = version;
+            this.stock = 10;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2}", this.Marca, this.Modelo, this.Version);
+        }
+
+        public void AgregarUnAuto()
+        {
+            this.stock = this.stock + 1;
+        }
+
+        //public void AgregarStock(int cantidad)
+        //{
+        //    this.stock = this.stock + cantidad;  
+        //}
+
+        public void QuitarStock(int cantidad)
+        {
+            this.stock = this.stock - cantidad;
+        }
+
+        //public bool QuitarStock(int cantidad, bool desactivar)
+        //{
+        //    this.stock = this.stock - cantidad;
+        //    if (desactivar)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //public bool Vendido()
+        //{
+        //    if (stock > o)
+        //    {
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        return true;
+        //    }
+        //}
+
+
+        public bool Vendido()
+        {
+            if (stock == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
